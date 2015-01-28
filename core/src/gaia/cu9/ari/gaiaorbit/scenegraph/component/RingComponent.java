e560a9f130fe8b5c22b2eec0e2b7051575ee8d0f
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2015-01-28 13:53:32 +0100

    Planets completely migrated to json

--- core/src/gaia/cu9/ari/gaiaorbit/scenegraph/component/RingComponent.java
+++ core/src/gaia/cu9/ari/gaiaorbit/scenegraph/component/RingComponent.java
@@ -1,22 +1,23 @@
 package gaia.cu9.ari.gaiaorbit.scenegraph.component;
 
 public class RingComponent {
-    public int innerRadius, outerRadius, divisions;
+    public int divisions;
+    public float innerRadius, outerRadius;
 
     public RingComponent() {
 
     }
 
-    public void setInnerRadius(int innerRadius) {
+    public void setInnerradius(Float innerRadius) {
 	this.innerRadius = innerRadius;
     }
 
-    public void setOuterRadius(int outerRadius) {
+    public void setOuterradius(Float outerRadius) {
 	this.outerRadius = outerRadius;
     }
 
-    public void setDivisions(int divisions) {
-	this.divisions = divisions;
+    public void setDivisions(Long divisions) {
+	this.divisions = divisions.intValue();
     }
 
 }
