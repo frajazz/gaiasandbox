4f7b3594e1d815986c9ce33badc055eca0d58950
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2015-04-15 12:56:04 +0200

    Fixed conflicting gaiasandbox.desktop from deb and rpm

--- desktop/exe/create-shortcut-deb.sh
+++ desktop/exe/create-shortcut-deb.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-#
-# Creates the .desktop shortcut in Debain systems
-#
-
-# Copy shortcut to apps folder
-cp /opt/GaiaSandbox/gaiasandbox.desktop $HOME/.local/share/applications
+# Copy shortcut to applications folder
+cp $I4J_INSTALL_LOCATION/gs.desktop $HOME/.local/share/applications/gaiasandbox.desktop
 
 # global.properties file permissions
-chmod ugo+w /opt/GaiaSandbox/conf/global.properties
+chmod ugo+w $I4J_INSTALL_LOCATION/conf/global.properties
--- desktop/exe/create-shortcut-rpm.sh
+++ desktop/exe/create-shortcut-rpm.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-#
-# Creates the .desktop shortcut in RPM based distros
-#
-
-# Copy shortcut to apps folder
-cp /opt/GaiaSandbox/gaiasandbox.desktop /usr/local/share/applications
+# Copy shortcut to applications folder
+cp $I4J_INSTALL_LOCATION/gs.desktop /usr/local/share/applications/gaiasandbox.desktop
 
 # global.properties file permissions
-chmod ugo+w /opt/GaiaSandbox/conf/global.properties
+chmod ugo+w $I4J_INSTALL_LOCATION/conf/global.properties
