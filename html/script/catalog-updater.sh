16b2abf5f7baaaec411681a7a0ebeb9ef0ea6a48
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2015-08-25 14:20:07 +0200

    No need to give read permissions in catalog updater script

--- html/script/catalog-updater.sh
+++ html/script/catalog-updater.sh
@@ -10,8 +10,4 @@ SERVER=hercules
 
 # Copy catalog file to server via SCP
 echo "Copying $FILE to $SERVER host"
-CMD2=`sshpass -f ./passfile scp $FILE "$USER@$SERVER:/work/www-staff/html/gaiasandbox/fov/android/assets/data/hygxyz.bin"`
-
-# Grant read permissions
-echo "Granting read permissions to $USER@$SERVER:$SERV_FOLDER/hygxyz.bin"
-CMD3=`sshpass -f ./passfile ssh "$USER@$SERVER" 'chmod ugo+r /work/www-staff/html/gaiasandbox/fov/android/assets/data/hygxyz.bin && exit'`
+CMD2=`sshpass -f ./passfile scp $FILE "$USER@$SERVER:/work/www-staff/html/gaiasandbox/focalplane/assets/data/hygxyz.bin"`
