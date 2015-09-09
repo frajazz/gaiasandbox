ada250e0909d15fa3c147d3078bbbfb54f20e78d
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2015-09-09 16:39:16 +0200

    Looks like everything works well. Added jre to windows installers. Fixed some issues with the build scripts. This finally fixes #2

--- desktop/exe/gaiasandbox.bat
+++ desktop/exe/gaiasandbox.bat
@@ -1,1 +1,5 @@
-java -Xmx512m -XX:+UseParallelGC -jar gaiasandbox.jar
+::
+:: Gaia Sandbox start script.
+::
+
+java -Xmx512m -XX:+UseParallelGC -cp "lib/*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSandboxDesktop
